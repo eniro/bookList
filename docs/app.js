a23eb5d914a570968eda82adbdb65470eaec962e
Iro Enezli <enezliro@gmail.com>
2018-08-28 12:59:32 +0300

    added remove books choice

--- docs/app.js
+++ docs/app.js
@@ -42,6 +42,13 @@ UI.prototype.showAlert = function(message, className) {
     }, 3000);
 }
 
+// Delete Book
+UI.prototype.deleteBook = function(target){
+    if(target.className === 'delete'){
+        target.parentElement.parentElement.remove();
+    }
+}
+
 
 // Clear Fields
 UI.prototype.clearFields = function(){
@@ -50,7 +57,7 @@ UI.prototype.clearFields = function(){
     document.getElementById('isbn').value = '';
 }
 
-// Event listeners
+// Event listeners for add book
 document.getElementById('book-form').addEventListener('submit', function(e){
     // Get form values
     const title = document.getElementById('title').value;
@@ -78,7 +85,20 @@ document.getElementById('book-form').addEventListener('submit', function(e){
         ui.clearFields();
     }
 
-   
+        e.preventDefault();
+});
+
+// Event Listener for Delete
+document.getElementById('book-list').addEventListener('click', function(e){
+    
+    // Instantiate UI
+    const ui = new UI();
+    
+    // Delete Book
+    ui.deleteBook(e.target);
+
+    // Show message
+    ui.showAlert('Book Removed!', 'success');
 
     e.preventDefault();
-})
+});
